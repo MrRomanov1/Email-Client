74ca770be3a8af169873f2d94688295da426164c
MrRomanov1 <piotr.romanczak@gmail.com>
2021-02-21 21:24:17 +0100

    Initial commit

---
+++ src/pl/piotr_romanczak/view/Launcher.java
@@ -0,0 +1,20 @@
+package pl.piotr_romanczak.view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+}
