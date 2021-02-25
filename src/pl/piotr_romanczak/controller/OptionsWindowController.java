8fd6caa022972b528ac67767c2bf6a4980017b54
MrRomanov1 <piotr.romanczak@gmail.com>
2021-02-25 21:26:10 +0100

    Added base options menu functionalities

--- src/pl/piotr_romanczak/controller/OptionsWindowController.java
+++ src/pl/piotr_romanczak/controller/OptionsWindowController.java
@@ -1,12 +1,22 @@
 package pl.piotr_romanczak.controller;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
 import pl.piotr_romanczak.EmailManager;
+import pl.piotr_romanczak.view.ColorTheme;
+import pl.piotr_romanczak.view.FontSize;
 import pl.piotr_romanczak.view.ViewFactory;
 
-public class OptionsWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Stack;
+
+public class OptionsWindowController extends BaseController implements Initializable {
 
     public OptionsWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
@@ -16,17 +26,60 @@ public class OptionsWindowController extends BaseController {
     private Slider fontSizePicker;
 
     @FXML
-    private ChoiceBox<?> themePicker;
+    private ChoiceBox<ColorTheme> themePicker;
 
 
     @FXML
-    void ApplyButtonAction() {
-
+    void applyButtonAction() {
+        viewFactory.setColorTheme(themePicker.getValue());
+        viewFactory.setFontSize(FontSize.values()[(int)(fontSizePicker.getValue())]);
+        System.out.println(viewFactory.getColorTheme());
+        System.out.println(viewFactory.getFontSize());
+        viewFactory.updateStyles();
     }
 
     @FXML
-    void CancelButtonAction() {
+    void cancelButtonAction() {
+        Stage stage = (Stage) fontSizePicker.getScene().getWindow();
+        viewFactory.closeStage(stage);
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setUpThemePicker();
+        setUpFontSizePicker();
+    }
+
+    private void setUpThemePicker() {
+        themePicker.setItems(FXCollections.observableArrayList(ColorTheme.values()));
+        themePicker.setValue(viewFactory.getColorTheme());
+    }
+
+    private void setUpFontSizePicker() {
+        fontSizePicker.setMin(0);
+        fontSizePicker.setMax(FontSize.values().length - 1);
+        fontSizePicker.setValue(viewFactory.getFontSize().ordinal());
+        fontSizePicker.setMajorTickUnit(1);
+        fontSizePicker.setMinorTickCount(0);
+        fontSizePicker.setBlockIncrement(1);
+        fontSizePicker.setSnapToTicks(true);
+        fontSizePicker.setShowTickMarks(true);
+        fontSizePicker.setShowTickLabels(true);
+        fontSizePicker.setLabelFormatter(new StringConverter<Double>() {
+            @Override
+            public String toString(Double object) {
+                int i = object.intValue();
+                return FontSize.values()[i].toString();
+            }
+
+            @Override
+            public Double fromString(String string) {
+                return null;
+            }
+        });
+        fontSizePicker.valueProperty().addListener((obs, oldVal, newVal) -> {
+            fontSizePicker.setValue(newVal.intValue());
+        });
+    }
 }
