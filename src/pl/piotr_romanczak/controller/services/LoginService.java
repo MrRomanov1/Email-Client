149daaa4bedd4a98f14812593437e0ab85ea67d9
MrRomanov1 <piotr.romanczak@gmail.com>
2021-03-04 21:57:34 +0100

    Load folders structure from email server

--- src/pl/piotr_romanczak/controller/services/LoginService.java
+++ src/pl/piotr_romanczak/controller/services/LoginService.java
@@ -31,6 +31,7 @@ public class LoginService extends Service<EmailLoginResult> {
                     emailAccount.getAddress(),
                     emailAccount.getPassword());
             emailAccount.setStore(store);
+            emailManager.addEmailAccount(emailAccount);
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
             return EmailLoginResult.FAILED_BY_UNEXPECTED_ERROR;
